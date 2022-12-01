e3fe631192d36f6e101aa86d38a2fba85610952d
yango <o3.yang@hotmail.com>
2022-12-01 15:10:39 +0000

    update get dts data

---
+++ cookbook-prj/dts/.tmp_versions/dt-data.mod
@@ -0,0 +1,3 @@
+/home/firefly/Documents/kernel-module-dev/cookbook-prj/dts/dt-data.ko
+/home/firefly/Documents/kernel-module-dev/cookbook-prj/dts/dt-data.o
+
--- cookbook-prj/dts/dt-data.c
+++ cookbook-prj/dts/dt-data.c
@@ -16,9 +16,73 @@ MODULE_PARM_DESC(path, "a device tree pathname " \
 /*
  * Local functions
  */
-static void print_property_u32(struct device_node *node, const cahr * name){
+
+static void print_property_u32(struct device_node *node, const char * name){
     u32 val32;
     if(of_property_read_u32(node, name, &val32) == 0){
         pr_info(" \%s = %d\n", name, val32);
     }
 }
+
+static void print_property_string(struct device_node *node, const char *name){
+    const char *str;
+    if(of_property_read_string(node, name, &str) == 0){
+        pr_info(" \%s = \%s\n", name, str);
+    }
+}
+static void print_main_prop(struct device_node *node){
+    pr_info("+ node = %s\n", node->full_name);
+    print_property_u32(node, "#address-cells");
+    print_property_u32(node, "#size-cells");
+    print_property_u32(node, "reg");
+    print_property_string(node, "name");
+    print_property_string(node, "compatible");
+    print_property_string(node, "status");
+}
+
+static int __init dt_data_init(void){
+    
+    struct device_node *node, *child;
+    struct property *prop;
+    
+    
+    pr_info("path = \"%s\"", path);
+
+    /* Find node by its pathname */
+    node = of_find_node_by_path(path);
+    if(!node){
+        pr_err("failed to find device-tree node \"%s\"\n", path);
+        return -ENODEV;
+    }
+    pr_info("device-tree node found.\n");
+
+    pr_info("now getting main properties...\n");
+    print_main_prop(node);
+
+    pr_info("now move through all properties...\n");
+    for_each_property_of_node(node, prop)
+        pr_info("-> %s\n", prop->name);
+
+    pr_info("now move through children...\n");
+    for_each_child_of_node(node, child)
+        print_main_prop(child);
+    
+    /* Force module unloading ... */
+    //return -EINVAL;
+    return 0;
+}
+
+static void __exit dt_data_exit(void){
+    
+}
+
+module_init(dt_data_init);
+module_exit(dt_data_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Rodolfo Yang");
+MODULE_DESCRIPTION("Inspect device tree ");
+MODULE_VERSION("0.1");
+
+
+
